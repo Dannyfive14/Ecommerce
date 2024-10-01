59d2c0e1a204e6f04a472fffc9dfd246ac206505
Carlos Figueroa <dannyfigueroav14@gmail.com>
2024-10-01 10:07:20 -0500

    "Added event listeners and functions for cart functionality in script.js, and updated HTML attributes in tienda.html"

--- script/script.js
+++ script/script.js
@@ -6,3 +6,39 @@ const listaProductos = document.querySelector('#lista-productos');
 let articulosCarrito = [];
 
 cargarEventListeners()
+
+//functions
+
+function cargarEventListeners(){
+    //Cuando agregas un producto presionando "agregar al carrito"
+    listaProductos.addEventListener('click', agregarProducto);
+
+    //Elimina los productos del carrito
+    carrito.addEventListener('click', eliminarProducto);
+
+    //Vacia el carrito
+    vaciarCarritoBtn.addEventListener('click', ()=>{
+        articulosCarrito = [];//Resetear el arreglo
+
+        limpiarHTML();//Eliminar todo el HTML
+    });
+
+}
+
+function agregarProducto (e){
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito')){
+        const productoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCarrito(productoSeleccionado);
+    }
+}
+
+function eliminarProducto(e){
+    if(e.target.classList.contains('borrar-producto')){
+        const productoId = e.target.getAttribute('data-id');
+
+        //Elimina el arreglo de articulosCarrito por el data-id
+        articulosCarrito = articulosCarrito.filter(curso => curso.id !== curosId);
+    }else{
+    }
+}
